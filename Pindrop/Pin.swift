c6468d9581df39ffba8578877c8db86c0be39ce8
Will Murat Said <will.said7@gmail.com>
2018-09-27 22:45:10 -0400

    updated storyboard, screenshots, and Pin statics

--- Pindrop/Pin.swift
+++ Pindrop/Pin.swift
@@ -8,31 +8,9 @@
 
 import Foundation
 import MapKit
-import CoreData
-
 
 
 class Pin: NSObject, Codable {
-    
-    // MARK: - STATIC
-    static let pinsKey = "pins"
-    
-    static func save(pins: [Pin]) {
-        let data = try? JSONEncoder().encode(pins)  // json encoding
-        UserDefaults.standard.set(data, forKey: pinsKey)
-    }
-    
-    static func loadPins() -> [Pin]? {
-        if let data = UserDefaults.standard.value(forKey: pinsKey) as? Data,  // UserDefaults
-            let pins = try? JSONDecoder().decode([Pin].self, from: data) {     // json decoding
-        
-            return pins
-        }
-        return nil
-    }
-    
-    
-    // MARK: - INSTANCE
     let name: String
     let caption: String
     let latitude: Double
@@ -43,6 +21,25 @@ class Pin: NSObject, Codable {
         self.caption = description
         self.latitude = latitude
         self.longitude = longitude
+    }
+}
+
+
+extension Pin {
+    static let pinsKey = "pins"
+    
+    static func save(pins: [Pin]) {
+        let data = try? JSONEncoder().encode(pins)  // json encoding
+        UserDefaults.standard.set(data, forKey: pinsKey)
+    }
+    
+    static func loadPins() -> [Pin]? {
+        if let data = UserDefaults.standard.value(forKey: pinsKey) as? Data,  // UserDefaults
+            let pins = try? JSONDecoder().decode([Pin].self, from: data) {     // json decoding
+            
+            return pins
+        }
+        return nil
     }
 }
 
